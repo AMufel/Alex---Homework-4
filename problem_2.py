3424964ee4c0a53a96fb92704af5d40c37a5c000
Alex M <36176961+AMufel@users.noreply.github.com>
2018-03-29 23:07:32 -0400

    Add files via upload

--- problem_2.py
+++ problem_2.py
@@ -1,14 +1,8 @@
 my_movies = {'The Saint', '5th Element', 'Ace Ventura', 'Forrest Gump'}
 
-friends_movies = {'Independence Day', 'Ferris Buelers Day Off', 'Major League', 'Forrest Gump'}
+friend_movies = {'Independence Day', 'Ferris Buelers Day Off', 'Major League', 'Forrest Gump'}
 
-def common_movies(movie_list1, movie_list2):
-    if len(my_movies & friends_movies) > 0:
-        print('These are the movies in common:\n')
-    for movie in (movie_list1 & movie_list2):
-        print('movie')
-    else:
-        print('There are no movies in common:\n')
-
-
-common_movies(my_movies, friends_movies)
+common_movies = my_movies.intersection(friend_movies)
+only_mymovies = my_movies.difference(friend_movies)
+print(common_movies)
+print(friend_movies)
